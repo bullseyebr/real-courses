a8ac5900ff88f8fabb74aaa745d0c597854bf655
Leosharkdev <sharkdevjf@gmail.com>
2023-05-29 12:29:09 -0300

    create navbar

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Real Cursos</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+  <header>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <img src="img/logo-real-cursos.png" alt="Real Grana Cursos" class="navbar-brand" width="10%" height="10%">
+
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle d-flex justify-content-end " href="#" id="avatarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <img src="img/avatar.png" alt="Avatar" class="avatar" height="20%" width="20%">
+            </a>
+            <div class="dropdown-menu" aria-labelledby="avatarDropdown">
+              <a class="dropdown-item" href="#">Meu perfil</a>
+              <a class="dropdown-item" href="#">Ajuda</a>
+              <a class="dropdown-item" href="#">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </div>
+
+  </header>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
