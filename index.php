a843d853be1118a0cfb5d8158ba897bba3614890
Leosharkdev <sharkdevjf@gmail.com>
2023-05-29 23:49:13 -0300

    finish navbar admin

--- index.php
+++ index.php
@@ -7,31 +7,27 @@
   <title>Real Cursos</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="index.css">
+  <script src="index.js"></script>
 </head>
 
 <body>
   <header>
-    <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-      <img src="img/logo-real-cursos.png" alt="Real Grana Cursos" class="navbar-brand" width="10%" height="10%">
-
-      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle d-flex justify-content-end " href="#" id="avatarDropdown" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              <img src="img/avatar.png" alt="Avatar" class="avatar" height="20%" width="20%">
-            </a>
-            <div class="dropdown-menu" aria-labelledby="avatarDropdown">
-              <a class="dropdown-item" href="#">Meu perfil</a>
-              <a class="dropdown-item" href="#">Ajuda</a>
-              <a class="dropdown-item" href="#">Logout</a>
-            </div>
-          </li>
-        </ul>
-      </div>
+    <nav class="navbar">
+      <div class="navbar-avatar" onclick="toggleDropdown()">
+        <img src="img/avatar.png" alt="" width="10%" height="10%">
+        <ul class="navbar-menu" id="dropdown-menu">
+        <li class="navbar-item">
+          <div class="dropdown navbar-item">
+        <li><a class="dropdown-item" href="#">Meu perfil</a></li>
+        <li><a class="dropdown-item" href="#">Ajuda</a></li>
+        <li><a class="dropdown-item" href="#">Logout</a></li>
+        </div>
+        </li>
+      </ul>
     </nav>
-    </div>
+      </div>
+
 
   </header>
 
