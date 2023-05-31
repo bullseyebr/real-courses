6af9151ab4c2f1309f626506844c108fc7996598
Leosharkdev <sharkdevjf@gmail.com>
2023-05-31 13:33:14 -0300

    home section

---
+++ home.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>home Administrativa</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+  <section class="home-bg pad-section">
+    <h1 class="text-white">Home Administrativa</h1>
+    <div class="container">
+    <div class="row">
+      <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card m-4 shadow-sm">
+          <div class="card-body home-bg">
+            <h5 class="card-title text-white">Gerenciar Cursos</h5>
+            <p class="card-text text-white">Veja seus cursos disponiveis</p>
+            <a href="#" class="btn btn-primary">Acessar Cursos</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card m-4 shadow-sm">
+          <div class="card-body home-bg">
+            <h5 class="card-title text-white">Gerenciar Categorias</h5>
+            <p class="card-text text-white">Veja todas categorias de cursos disponiveis</p>
+            <a href="#" class="btn btn-primary">Acessar Categorias</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
