b08592a8b4e4b0cc6ea04e190efb6e96f6b1cbaf
fcamposleonardo <camposleonardo18@gmail.com>
2023-06-01 18:59:31 -0300

    create footer with social media Icons and copyright

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Real Cursos</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <script src="https://kit.fontawesome.com/cafc52a010.js" crossorigin="anonymous"></script>
+  <script src="index.js"></script>
+</head>
+<body>
+    
+</body>
+
+<footer class="fixed-bottom">
+    <div class="text-center p-3">
+        <a href="https://www.facebook.com/profile.php?id=100088413534772"><i class="fa-brands fa-facebook" style='font-size:30px'></i></a>
+        <a href="https://www.instagram.com/real.grana/"><i class="fa-brands fa-instagram" style='font-size:30px'></i></a>
+        <a href="https://twitter.com/RealGrana"><i class="fa-brands fa-twitter" style='font-size:30px'></i></a>
+        <a href="https://github.com/bullseyebr"><i class="fa-brands fa-github" style='font-size:30px'></i></a>        
+    </div>
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >Real Grana © | Uma marca Bullseye ® | 2020-2023
+    </div>
+</footer>
