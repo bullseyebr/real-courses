f944ba0463903df037183564d255c4ddbe3f04ad
fcamposleonardo <camposleonardo18@gmail.com>
2023-06-11 21:10:55 -0300

    sidebar created

---
+++ sidebar.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="bg-dark col-auto col-md-2 min-vh-100">
+                <div class="bg-dark p-2">
+                    <a class="d-flex text decoration-none mt-1 align-items-center text-white">
+                        <a  img src="img/avatar.png" alt="" width="10%" height="10%">></a>
+                    <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                        <img src="img/logo-real-cursos.png" alt="" width="20%" height="20%">
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-white">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#"class="nav-link text-white">Cursos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#"class="nav-link text-white">Categorias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#"class="nav-link text-white">Item</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <script src="./bootstrap/js/bootstrap.bundle.js"></script>
+</body>
